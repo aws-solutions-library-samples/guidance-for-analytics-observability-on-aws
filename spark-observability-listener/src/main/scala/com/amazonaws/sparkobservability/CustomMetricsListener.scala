6d0415215f082d53ad0d82f9565339c9d16cc4fd
Narcisse Zekpa <nzzekpa@amazon.com>
2023-01-27 15:35:04 -0500

    Custom Metrics Listener V0

--- spark-observability-listener/src/main/scala/com/amazonaws/sparkobservability/CustomMetricsListener.scala
+++ spark-observability-listener/src/main/scala/com/amazonaws/sparkobservability/CustomMetricsListener.scala
@@ -1,7 +1,7 @@
 package com.amazonaws.sparkobservability
 
-import org.apache.spark.scheduler.{SparkListenerStageCompleted, SparkListener, SparkListenerJobStart}
-
+import org.apache.spark.scheduler.{SparkListenerStageCompleted, SparkListener, SparkListenerJobStart, SparkListenerTaskEnd}
+import scala.collection.mutable.Map
 
 class CustomMetricsListener extends SparkListener {
 
@@ -11,5 +11,45 @@ class CustomMetricsListener extends SparkListener {
 
   override def onStageCompleted(stageCompleted: SparkListenerStageCompleted): Unit = {
     println(s"Stage ${stageCompleted.stageInfo.stageId} completed with ${stageCompleted.stageInfo.numTasks} tasks.")
+  }
+
+  override def onTaskEnd(taskEnded: SparkListenerTaskEnd){
+    val metrics = collectMetrics(taskEnded)
+    println(s"Metrics collected -> ${metrics}.")
+  }
+
+  def collectMetrics(taskEnded: SparkListenerTaskEnd): Map[Any, Any] = {
+    val stageId = taskEnded.stageId
+    val taskInfo = taskEnded.taskInfo
+    val taskId = taskEnded.taskInfo.id
+    val host = taskInfo.host
+    val status = taskEnded.reason
+    val executorId = taskInfo.executorId
+    val partitionId = taskInfo.partitionId
+    
+    val metrics = taskEnded.taskMetrics
+
+    var inputSizeRead = 0.0
+    var inputRecordsRead = 0.0
+    var runTime = 0.0
+
+    if(metrics != null){
+      runTime = metrics.executorRunTime
+      val inputMetrics = metrics.inputMetrics
+      if(inputMetrics != null){
+        inputSizeRead =  inputMetrics.bytesRead
+        inputRecordsRead =  inputMetrics.recordsRead
+      }
+    }
+
+    return Map(
+      "stageId"->stageId,
+      "taskId"->taskId,
+      "executorId"->executorId,
+      "partitionId"->partitionId,
+      "inputSizeRead"->inputSizeRead,
+      "inputRecordsRead"->inputRecordsRead,
+      "runTime"->runTime
+    )
   }
 }
