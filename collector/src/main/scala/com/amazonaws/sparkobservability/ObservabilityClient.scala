ef15121b8dc33df5ad0d5e1d5b06020f319610f1
Vincent Gromakowski <gromav@amazon.com>
2023-04-07 18:50:17 +0200

    refactor observability client for data prepper

--- collector/src/main/scala/com/amazonaws/sparkobservability/ObservabilityClient.scala
+++ collector/src/main/scala/com/amazonaws/sparkobservability/ObservabilityClient.scala
@@ -1,50 +1,74 @@
 package com.amazonaws.sparkobservability
 
-import org.apache.http.HttpHost
-import org.apache.http.conn.ssl.{NoopHostnameVerifier, TrustAllStrategy}
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder
-import org.apache.http.ssl.SSLContextBuilder
 import org.apache.logging.log4j.core.LogEvent
-import org.opensearch.action.index.IndexRequest
-import org.opensearch.client.{RequestOptions, RestClient, RestClientBuilder, RestHighLevelClient}
 import org.slf4j.LoggerFactory
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
+import software.amazon.awssdk.auth.signer.Aws4Signer
+import software.amazon.awssdk.auth.signer.params.Aws4SignerParams
+import software.amazon.awssdk.core.sync.RequestBody
+import software.amazon.awssdk.http.apache.ApacheHttpClient
+import software.amazon.awssdk.http.{HttpExecuteRequest, SdkHttpFullRequest, SdkHttpMethod}
+import software.amazon.awssdk.regions.Region
 
-import java.util
+import java.io.ByteArrayInputStream
+import java.net.URI
+import java.util.HashMap
+import scala.util.control.Exception
+import com.google.gson.Gson
 
 object ObservabilityClient {
 
   private val logger = LoggerFactory.getLogger(this.getClass.getName)
+  private val gson = new Gson
+  private val credentialsProvider = DefaultCredentialsProvider.create
+  private val signer = Aws4Signer.create
+  private val client = ApacheHttpClient.builder.build
+  private val params: Aws4SignerParams = Aws4SignerParams.builder()
+    .awsCredentials(credentialsProvider.resolveCredentials())
+    .signingName("localhost")
+    .signingRegion(Region.US_WEST_2)
+    .build()
 
-  lazy val opensearchClient: RestHighLevelClient = {
-    val builder = RestClient.builder(new HttpHost("localhost", 9200, "http"))
-      .setHttpClientConfigCallback(
-        new RestClientBuilder.HttpClientConfigCallback() {
-          @Override
-          override def customizeHttpClient(httpClientBuilder: HttpAsyncClientBuilder): HttpAsyncClientBuilder = {
-            httpClientBuilder
-              .setSSLContext(new SSLContextBuilder().loadTrustMaterial(null, TrustAllStrategy.INSTANCE).build())
-              .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
-          }
-        }
-      )
-    new RestHighLevelClient(builder)
+  def sendContent(content: String): Unit = {
+    val request = SdkHttpFullRequest.builder
+      .contentStreamProvider(RequestBody.fromString("[" + content + "]").contentStreamProvider())
+      .method(SdkHttpMethod.POST)
+      .putHeader("Content-Length", Integer.toString(content.length()+2))
+      .putHeader("Content-Type", "application/json")
+      .protocol("https")
+      .uri(URI.create("http://localhost:2021/log/ingest"))
+      .build
+
+    val signedRequest = signer.sign(request, params)
+    val executeRequest = HttpExecuteRequest.builder
+      .request(signedRequest)
+      .build
+
+    logger.info("request sent to data prepper " + executeRequest.contentStreamProvider.get.toString)
+    val response = client.prepareRequest(executeRequest).call
+    logger.info("data prepper response :" + response.httpResponse.statusText())
+    logger.info("data prepper response :" + response.httpResponse.statusCode)
+    if (response.httpResponse.statusCode != 200) throw new Exception("error sending to data prepper")
   }
 
   def send(event: LogEvent): Unit = {
-    val request = new IndexRequest("spark-logs")
-    val requestContent = new util.HashMap[String, String](){
+    val requestContent = new HashMap[String, Any](){
       put("message", event.getMessage.toString)
       put("logger", event.getLoggerFqcn)
       put("level", event.getLevel.toString)
     }
-    request.source(requestContent)
-    opensearchClient.index(request, RequestOptions.DEFAULT)
+    logger.info("sending log " + requestContent.toString)
+    logger.info("sending log as string " + event.toString)
+    sendContent(event.toString)
   }
 
   def send(metrics: CustomMetrics): Unit = {
-    val request = new IndexRequest("spark-metrics")
-    request.source(metrics.toMap())
-    logger.info("sending metrics " + metrics.toMap().toString())
-    opensearchClient.index(request, RequestOptions.DEFAULT)
+    val jsonString = gson.toJson(metrics)
+    val requestContent = metrics.toMap
+    logger.info("sending metrics " + requestContent.toString)
+    logger.info("sending metrics as JSON string " + jsonString)
+    sendContent(jsonString)
   }
+
+
 }
