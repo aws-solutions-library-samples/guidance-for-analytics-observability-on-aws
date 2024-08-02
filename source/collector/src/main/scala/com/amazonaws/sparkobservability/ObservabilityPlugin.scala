910b08a7f1cb8092f3ab1b0f753c8e20ee279f14
avichaym <martziano@gmail.com>
2024-08-02 14:19:20 +0300

    adding a plugin interface (needs to be used as a spark conf option) to make sure we instantiate with context

---
+++ source/collector/src/main/scala/com/amazonaws/sparkobservability/ObservabilityPlugin.scala
@@ -0,0 +1,32 @@
+package com.amazonaws.sparkobservability
+
+import com.amazonaws.sparkobservability.SparkUtility
+import org.apache.spark.api.plugin.{DriverPlugin, ExecutorPlugin, PluginContext, SparkPlugin}
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.execution.ui.SQLAppStatusListener
+import org.apache.spark.SparkContext
+import org.apache.spark.status._
+
+import java.util
+import scala.collection.JavaConverters.mapAsJavaMapConverter
+
+/**
+ * Helps initializing the CustomMetricsListener with spark context.
+ */
+class ObservabilityPlugin extends SparkPlugin {
+
+  override def driverPlugin(): DriverPlugin = new ObservabilityDriverPlugin()
+  override def executorPlugin(): ExecutorPlugin = null
+
+
+  class ObservabilityDriverPlugin extends DriverPlugin with Logging {
+    var sc: SparkContext = null
+    var sqlFunction:  () => Option[SQLAppStatusListener] = null
+
+    override def init(sc: SparkContext, pluginContext: PluginContext): util.Map[String, String] = {
+      this.sqlFunction = SparkUtility.getSqlListener(sc)
+      this.sc = sc
+      this.sc.addSparkListener(new CustomMetricsListener(sc, sqlFunction))
+      Map[String, String]().asJava    }
+  }
+}
